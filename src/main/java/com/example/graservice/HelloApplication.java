2f0d387a0ab456632055628e1d71c294888edee6
revlo9 <dedowicz73@gmail.com>
2021-10-27 12:09:09 +0200

    Added backend (not working as it is)

---
+++ src/main/java/com/example/graservice/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.graservice;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
