c4b3e44eff1c11994866cb1633184a19c0ab6329
Jakub Bembenek <jakub.bembenekk@gmail.com>
2022-01-05 08:56:00 +0100

    Changed auth token response from text to json form

--- src/main/java/com/example/graservice/resources/AuthenticationResource.java
+++ src/main/java/com/example/graservice/resources/AuthenticationResource.java
@@ -6,10 +6,12 @@ import com.example.graservice.services.AuthenticationService;
 import jakarta.annotation.security.PermitAll;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
+import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
 
 @RequestScoped
 @Path("/auth")
@@ -27,7 +29,10 @@ public class AuthenticationResource {
         if (user != null) {
             String userName = user.getFirstName().concat(" ".concat(user.getLastName()));
             String token = authenticationService.createToken(user.getId(), userName, user.isAdmin());
-            return Response.ok(token).build();
+            JsonObject jsonToken =  Json.createObjectBuilder()
+                    .add("jwt", token)
+                    .build();
+            return Response.ok(jsonToken).build();
         }
         return Response.status(Response.Status.UNAUTHORIZED).build();
     }
